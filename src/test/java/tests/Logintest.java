b402275c87cf3e2b163583b7c0e72583478d68cd
sampath <sampath@sampaths-MacBook-Pro.local>
2026-02-15 10:56:48 +0530

    Added login locators

--- src/test/java/tests/Logintest.java
+++ src/test/java/tests/Logintest.java
@@ -1,4 +1,12 @@
 package tests;
 
+import org.openqa.selenium.By;
+
 public class Logintest {
+
+    private By email_text = By.xpath("idtoken");
+    private By password_btn = By.xpath("idtoken");
+
+    private By loginBtn = By.xpath("idtoken");
+
 }
