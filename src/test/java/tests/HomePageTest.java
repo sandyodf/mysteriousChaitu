22622f95c2dd34b96142914b58bb5e648afbca5f
sampath <sampath@sampaths-MacBook-Pro.local>
2026-02-15 11:08:00 +0530

    added browser util

--- src/test/java/tests/HomePageTest.java
+++ src/test/java/tests/HomePageTest.java
@@ -1,4 +1,10 @@
 package tests;
 
+import org.openqa.selenium.By;
+
 public class HomePageTest {
+    private By searchtext = By.xpath("idtoken");
+    private By searchtext_btn = By.xpath("idtoken");
+
+    private By listOfProducts = By.xpath("idtoken");
 }
